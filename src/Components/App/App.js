3f2471d6d0f9e915a27a889dfa0a7cc818c7c7fa
eldarhasandwich <hasanovicel@gmail.com>
2018-05-29 13:14:29 +1000

    added info to readme, made title look nicer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux'
-import './App.css';
+
+import * as pjson from '../../../package.json'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React-Redux</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1
+                    style={{textAlign:"center", marginBottom:"2px"}}
+                >
+                    <code>
+                        react-redux-boilerplate
+                    </code>
+                </h1>
+
+                <p
+                    style={{textAlign:"center", marginTop:"2px"}}
+                >
+                    <code>@{pjson.version}</code>
+                </p>
+
+                <p
+                    style={{textAlign:"center"}}
+                >
+                    If you're seeing this, everything is working!
+                </p>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return {}
+    return {}
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+    return {}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
